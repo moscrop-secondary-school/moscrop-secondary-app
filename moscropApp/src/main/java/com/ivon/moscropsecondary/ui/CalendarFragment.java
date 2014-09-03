1004128cc18cd48d0bf6b5abe39270814557d2ef
Ivon Liu <ivon.liu2011@gmail.com>
2014-09-02 18:07:51 -0700

    Preliminary JSON parsing

--- moscropApp/src/main/java/com/ivon/moscropsecondary/ui/CalendarFragment.java
+++ moscropApp/src/main/java/com/ivon/moscropsecondary/ui/CalendarFragment.java
@@ -10,15 +10,30 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.ivon.moscropsecondary.R;
+import com.ivon.moscropsecondary.util.Logger;
 import com.tyczj.extendedcalendarview.CalendarProvider;
 import com.tyczj.extendedcalendarview.Event;
 import com.tyczj.extendedcalendarview.ExtendedCalendarView;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Calendar;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 public class CalendarFragment extends Fragment {
+
+    public static final String MOSCROP_CALENDAR_JSON_URL = "http://www.google.com/calendar/feeds/moscroppanthers@gmail.com/public/full?alt=json&max-results=10&orderby=starttime&sortorder=descending";
 
     private int mPosition;
     private View mContentView;
@@ -43,6 +58,14 @@ public class CalendarFragment extends Fragment {
 
         mCalendarView = (ExtendedCalendarView) mContentView.findViewById(R.id.calendar);
         mCalendarView.setGesture(ExtendedCalendarView.LEFT_RIGHT_GESTURE);
+
+        // JSON Testing stuff
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doJsonStuff();
+            }
+        }).start();
 
     	return mContentView;
     }
@@ -91,5 +114,77 @@ public class CalendarFragment extends Fragment {
     private int getJulianDayFromCalendar(Calendar calendar) {
         TimeZone tz = TimeZone.getDefault();
         return Time.getJulianDay(calendar.getTimeInMillis(), TimeUnit.MILLISECONDS.toSeconds(tz.getOffset(calendar.getTimeInMillis())));
+    }
+
+    private void doJsonStuff() {
+
+        JSONObject main = getJsonObject(MOSCROP_CALENDAR_JSON_URL);
+        if(main == null) {
+            return;
+        }
+
+        try {
+            JSONObject feed = main.getJSONObject("feed");
+            JSONObject updated = feed.getJSONObject("updated");
+            String time = updated.getString("$t");
+            Logger.log("Updated time", time);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private JSONObject getJsonObject(String url) {
+
+        JSONObject resultObj = null;
+
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+
+        InputStream inputStream = null;
+        String resultStr = null;
+        try {
+
+            // Make sure status is OK
+            HttpResponse response = httpclient.execute(httpGet);
+            StatusLine status = response.getStatusLine();
+            if (status.getStatusCode() != HttpStatus.SC_OK) {
+                Logger.log("Status code", status.getStatusCode());
+                Logger.log("Reason", status.getReasonPhrase());
+                return null;
+            }
+
+            // Read response
+            HttpEntity entity = response.getEntity();
+            inputStream = entity.getContent();
+            // json is UTF-8 by default
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
+
+            // Build input stream into response string
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                sb.append(line).append("\n");
+            }
+            resultStr = sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                if(inputStream != null) {
+                    inputStream.close();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            resultObj = new JSONObject(resultStr);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return resultObj;
     }
 }
