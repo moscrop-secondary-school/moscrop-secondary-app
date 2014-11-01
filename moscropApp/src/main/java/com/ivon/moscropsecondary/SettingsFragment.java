e88adf4cb142194334d09e2a6c68df64337d897f
Ivon Liu <ivon.liu2011@gmail.com>
2014-10-31 22:34:15 -0700

    Fully functional tag chooser, just not linked to anything

--- moscropApp/src/main/java/com/ivon/moscropsecondary/SettingsFragment.java
+++ moscropApp/src/main/java/com/ivon/moscropsecondary/SettingsFragment.java
@@ -7,8 +7,10 @@ import android.preference.PreferenceFragment;
 import android.widget.Toast;
 
 import com.ivon.moscropsecondary.rss.RSSTagCriteria;
+import com.ivon.moscropsecondary.util.Logger;
 import com.ivon.moscropsecondary.util.Preferences;
 import com.ivon.moscropsecondary.util.ThemesUtil;
+import com.ivon.moscropsecondary.util.Util;
 
 import org.json.JSONException;
 
@@ -22,6 +24,8 @@ import java.util.Set;
 public class SettingsFragment extends PreferenceFragment
         implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
 
+    private MultiSelectListPreference mTagChooser;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +36,68 @@ public class SettingsFragment extends PreferenceFragment
         Preference theme = findPreference(Preferences.Keys.THEME);
         theme.setOnPreferenceChangeListener(this);
 
-        MultiSelectListPreference tagChooser = (MultiSelectListPreference) findPreference(Preferences.Keys.TAGS);
+        mTagChooser = (MultiSelectListPreference) findPreference(Preferences.Keys.TAGS);
+        updateTagsList();
+
+        Preference refresh = findPreference("refresh_tag_list");
+        refresh.setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        int titleRes = preference.getTitleRes();
+        if(titleRes == R.string.theme_selector_title) {
+            ThemesUtil.notifyThemeChanged();
+            Toast.makeText(getActivity(), "Theme will change when you exit settings", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        int titleRes = preference.getTitleRes();
+        if (titleRes == R.string.refresh_tag_list_title) {
+            Logger.log("Refresh clicked");
+            refreshTagsFromServer();
+            return true;
+        }
+        return false;
+    }
+
+    private void refreshTagsFromServer() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (Util.isConnected(getActivity())) {
+                    try {
+                        RSSTagCriteria.downloadTagListToStorage(getActivity());
+                        showResult(true);
+                        return;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                showResult(false);
+            }
+
+            private void showResult(final boolean success) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(
+                                getActivity(),
+                                success ? "Categories successfully updated" : "Categories update failed",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                        updateTagsList();
+                    }
+                });
+            }
+        });
+        thread.start();
+    }
+
+    private void updateTagsList() {
         String[] tagNamesArray;
         try {
             tagNamesArray = RSSTagCriteria.getTagNames(getActivity());
@@ -41,7 +106,7 @@ public class SettingsFragment extends PreferenceFragment
         } catch (JSONException e) {
             tagNamesArray = new String[] {};
         }
-        Set<String> existingSelectedValuesSet = tagChooser.getValues();
+        Set<String> existingSelectedValuesSet = mTagChooser.getValues();
         String[] existingSelectedValues = existingSelectedValuesSet.toArray(new String[existingSelectedValuesSet.size()]);
 
         List<String> newSelectedValues = new ArrayList<String>();
@@ -53,23 +118,8 @@ public class SettingsFragment extends PreferenceFragment
 
         Set<String> newSelectedValuesSet = new HashSet<String>(newSelectedValues);
 
-        tagChooser.setEntries(tagNamesArray);
-        tagChooser.setEntryValues(tagNamesArray);
-        tagChooser.setValues(newSelectedValuesSet);
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        int titleRes = preference.getTitleRes();
-        if(titleRes == R.string.theme_title) {
-            ThemesUtil.notifyThemeChanged();
-            Toast.makeText(getActivity(), "Theme will change when you exit settings", Toast.LENGTH_SHORT).show();
-        }
-        return true;
-    }
-
-    @Override
-    public boolean onPreferenceClick(Preference preference) {
-        return false;
+        mTagChooser.setEntries(tagNamesArray);
+        mTagChooser.setEntryValues(tagNamesArray);
+        mTagChooser.setValues(newSelectedValuesSet);
     }
 }
