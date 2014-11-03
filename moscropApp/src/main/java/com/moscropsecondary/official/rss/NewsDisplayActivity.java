c041aa8ed8fe05067000da80f31882205c0005bf
asdfasdfvful <asdfasdfvful@gmail.com>
2014-11-02 23:36:27 -0800

    Themed card activity

--- moscropApp/src/main/java/com/moscropsecondary/official/rss/NewsDisplayActivity.java
+++ moscropApp/src/main/java/com/moscropsecondary/official/rss/NewsDisplayActivity.java
@@ -4,19 +4,28 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
 
 import com.moscropsecondary.official.R;
 import com.moscropsecondary.official.ToolbarActivity;
+import com.moscropsecondary.official.util.ThemesUtil;
 
-public class NewsDisplayActivity extends ToolbarActivity {
+public class NewsDisplayActivity extends ToolbarActivity
+        implements ThemesUtil.ThemeChangedListener {
 	
 	public static final String EXTRA_URL = "url";
 	public static final String EXTRA_CONTENT = "content";
 	public static final String EXTRA_TITLE = "abTitle";
+    private boolean mThemeRequiresUpdate = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
+        int theme = ThemesUtil.getThemeResFromPreference(this);
+        setTheme(theme);
+        mThemeRequiresUpdate = false;   // We just set the latest theme
+        ThemesUtil.registerThemeChangedListener(this);
+
         super.onCreate(savedInstanceState);
 		
 		String url = getIntent().getStringExtra(EXTRA_URL) + "?m=1";
@@ -28,6 +37,15 @@ public class NewsDisplayActivity extends ToolbarActivity {
         		.replace(R.id.content_frame, NewsDisplayFragment.newInstance(url, htmlContent, title))
         		.commit();
 	}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mThemeRequiresUpdate) {
+            startActivity(new Intent(this, NewsDisplayActivity.class));
+            finish();
+        }
+    }
 
     @Override
     protected int getLayoutResource() {
@@ -56,5 +74,9 @@ public class NewsDisplayActivity extends ToolbarActivity {
             return true;
         }
         return super.onKeyDown(keyCode, keyEvent);
+    }
+    @Override
+    public void onThemeChanged() {
+        mThemeRequiresUpdate = true;
     }
 }
