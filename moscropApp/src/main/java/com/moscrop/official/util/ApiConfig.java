07b40ea1f5af24f53d3ad9c32202f3d11126bb32
Ivon Liu <ivon.liu2011@gmail.com>
2015-09-07 14:13:50 -0700

    Commit weird 'changes'

---
+++ moscropApp/src/main/java/com/moscrop/official/util/ApiConfig.java
@@ -0,0 +1,16 @@
+package com.moscrop.official.util;
+
+/**
+ * Created by ivon on 08/08/15.
+ */
+public class ApiConfig {
+
+    public static class Parse {
+        public static final String API_KEY = "TtFxol5uKm6piqaomYYvaTtezhRJQFZFRadc9qit";
+        public static final String CLIENT_KEY = "ZEam7fclttpmSY1LBTa5DFwXRTlB0hfrfyzgXeD5";
+    }
+
+    public static class Google {
+        public static final String API_KEY = "AIzaSyDQgD1es2FQdm4xTA1tU8vFniOglwe4HsE";
+    }
+}
